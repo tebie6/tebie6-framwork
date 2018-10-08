88105d64e46ad769b58105a5fa3786defff9c3e2
tebie6 <liumingyuphp@163.com>
2018-10-08 23:52:55 +0800

    database

--- common/config/database.php
+++ common/config/database.php
@@ -7,5 +7,21 @@
  */
 
 return [
-    
+
+    'driver'    => 'mysql',
+
+    'host'      => 'localhost',
+
+    'database'  => 'blog',
+
+    'username'  => 'root',
+
+    'password'  => '',
+
+    'charset'   => 'utf8',
+
+    'collation' => 'utf8_general_ci',
+
+    'prefix'    => ''
+
 ];
